88c9c5d0c944cfb6071149f64cd34354621ab4ab
jonathan1brine <jonathan1brine@gmail.com>
2026-07-10 14:56:24 +0930

    Correct ticketUrl string formatting
    Fix ticketUrl formatting by adding quotes.

--- js/gigs.js
+++ js/gigs.js
@@ -16,7 +16,7 @@ const GIGS = [
     venue: "The Gov Upstairs",
     city: "Adelaide, SA",
     event: "Sunday / Projector / Rusthaven / BLuehour",
-    ticketUrl: https://www.instagram.com/liveandlocaladelaide/,
+    ticketUrl: "https://www.instagram.com/liveandlocaladelaide/",
   },
   // ---- PAST ----
   {
